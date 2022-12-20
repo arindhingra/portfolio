637eb9fa9ad8603822b661f9a61834737343f4bc
arindhingra <arindhingra107@gmail.com>
2022-12-20 22:59:34 +0000

    GRASS

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script> 
+let count = 95; 
+</script>
+<button on:click = {() => count++}>{count} </button>
+
+<h1> arinrinrirnrirnrnrirnirinrinririnirninrinrinrinrinrinrinrinrinrinrinrin CLICK THE BUTTON ABOVE ^^^</h1>
+<p> heellooo aimigo</p>
+
+<style> 
+</style>
+
+<a href = "/about"> My About Page </a>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,4 @@
+<h1> ABOUAT ME</h1>
+<p> stop readin this bruahuhuuhuuhuhuh this is why u have no friends sutpid SUIII
+</p>
+<a href = "/"> My Home Page</a>
