9b8a85ac20b228cad2d0d44c1fc1f1f395154674
arindhingra <arindhingra107@gmail.com>
2022-12-20 23:44:00 +0000

    hiiii

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,28 @@
-<script> 
+<script>
+    import { blank_object, object_without_properties } from "svelte/internal";
+
+ 
 let count = 95; 
 </script>
 <button on:click = {() => count++}>{count} </button>
 
-<h1> arinrinrirnrirnrnrirnirinrinririnirninrinrinrinrinrinrinrinrinrinrinrin CLICK THE BUTTON ABOVE ^^^</h1>
-<p> heellooo aimigo</p>
+<h1> <b> Dhingra Portforlio </b> </h1>
+<p class = "redtext">  heellooo aimigo </p>
 
-<style> 
-</style>
 
 <a href = "/about"> My About Page </a>
+<!-- UR COOOOOOOOOOOOL -->
+<style> 
+h1{
+    color : green; 
+}
+.redtext{
+    color : red;
+}
+:global(body) {
+    background-color: black;
+    color: white;
+    font-family: sans-serif;
+}
+
+</style>
